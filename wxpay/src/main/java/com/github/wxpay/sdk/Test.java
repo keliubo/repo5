21435ffa6ed87c8abd5b9a129a1d5cbb0a7e3ac4
keliubo <44825112+keliubo@users.noreply.github.com>
2019-12-09 18:25:50 +0800

    Update Test.java

--- wxpay/src/main/java/com/github/wxpay/sdk/Test.java
+++ wxpay/src/main/java/com/github/wxpay/sdk/Test.java
@@ -34,6 +34,7 @@ public class Test {
         m.put("code_url", mapResult.get("code_url"));
         m.put("total_fee",money+"");
         m.put("out_trade_no",orderId);
+        //提交
         System.out.println("提交");
     }
 }
